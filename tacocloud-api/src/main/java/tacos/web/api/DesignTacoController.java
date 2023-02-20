d12751aac907d47822fd285d2aa72679f5c1ea27
WinsonHuang <hwangwc@qq.com>
2023-02-20 13:13:04 +0800

    Add postTaco at /design

--- tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -9,7 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import tacos.Taco;
@@ -23,6 +26,12 @@ public class DesignTacoController {
 
     public DesignTacoController(TacoRepository tacoRepo) {
         this.tacoRepo = tacoRepo;
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
     }
 
     @GetMapping("/recent")
