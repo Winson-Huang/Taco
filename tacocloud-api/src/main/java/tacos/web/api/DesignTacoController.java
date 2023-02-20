7f20024dffdaebaa52b4c8d62a0d6183be1d99b1
WinsonHuang <hwangwc@qq.com>
2023-02-20 16:01:52 +0800

    Complete 6.2.1

--- tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -1,9 +1,13 @@
 package tacos.web.api;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -35,11 +39,21 @@ public class DesignTacoController {
     }
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos() {
+    public Resources<Resource<Taco>> recentTacos() {
         PageRequest page = PageRequest.of(
             0, 12, Sort.by("createdAt").descending()
         );
-        return tacoRepo.findAll(page).getContent();
+        List<Taco> tacos = tacoRepo.findAll(page).getContent();
+
+        Resources<Resource<Taco>> recentResources = Resources.wrap(tacos);
+        recentResources.add(
+            ControllerLinkBuilder.linkTo(
+                ControllerLinkBuilder.methodOn(DesignTacoController.class)
+                .recentTacos()
+            ).withRel("recents")
+        );
+
+        return recentResources;
     }
 
     @GetMapping("/{id}")
